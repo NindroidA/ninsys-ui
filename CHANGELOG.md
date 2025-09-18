e7520130a0221cdec8f435958b8e3a5f0a17d2a0
NindroidA <andrew@larrycurtis.com>
2025-09-18 11:50:56 -0500

    dev update v1.0.7

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# v1.0.7
+- Forgor to add the index.ts in components directory.
+- Added eslint for consistency.
+
+# v1.0.0-1.0.6
+Initial setup of everything:
+- Migrated components from main react app.
+- GitHub workflow for publishing to npm.
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { Badge } from './Badge';
+export { Button } from './Button';
+export { Card } from './Card';
+export { FloatingElements } from './FloatingElements';
+export { GradientText } from './GradientText';
+export { Grid } from './Grid';
+export { Section } from './Section';
